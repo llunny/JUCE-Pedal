740faa85d2404b05f14e6ef408397935b5b08323
llunny <161387242+llunny@users.noreply.github.com>
2025-04-18 12:06:05 -0700

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -19,30 +19,30 @@
 class SmartPedalAudioProcessorEditor : public juce::AudioProcessorEditor, public juce::Button::Listener
 {
 public:
-    
-    
-    
+
+
+
     SmartPedalAudioProcessorEditor(SmartPedalAudioProcessor&);
     ~SmartPedalAudioProcessorEditor() override;
 
     //==============================================================================
     void paint(juce::Graphics&) override;
     void resized() override;
-    
-    
-    
-    
+
+
+
+
     void buttonClicked(juce::Button* button) override;
 
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SmartPedalAudioProcessor& audioProcessor;
-    
-    
+
+
     // adds Distortion Button
     juce::TextButton Distortion;
-    
+
     juce::TextButton Reverberation;
     juce::TextButton Overdrive;
     juce::TextButton Harmony;
